23d016769b1dfd170fb37b89f2e66ca7b59bd813
“managatsuwo” <mystknr.macbook13@gmail.com>
2021-03-13 08:42:18 +0000

    3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c47615b4325b0d8c32a00d11cc33663e025304e66492bda7169b7d4d22c0124bb104760a852be197178fa5cc7ccc8f0822650bdd9ccb6925d8b3ef21f9f80f21'
