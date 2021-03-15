8328c7d1887fbe5c07692d63799825da9ea777f1
“managatsuwo” <mystknr.macbook13@gmail.com>
2021-03-15 00:24:57 +0000

    5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
- root to:'homes#top'
- resources:post_images,only:[:new,:create,:index,:show,:destroy]
- 
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show,:show]
+
+
 end
